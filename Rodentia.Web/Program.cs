1e33198b7f07b85623db99e9c7fc1b5e76bc3c21
Manko Maria <mankomaria1410@gmail.com>
2026-03-13 02:18:14 +0200

    Added Serilog logging and StyleCop analyzers

--- Rodentia.Web/Program.cs
+++ Rodentia.Web/Program.cs
@@ -1,20 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Rodentia.Data;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddDbContext<RodentiaDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
